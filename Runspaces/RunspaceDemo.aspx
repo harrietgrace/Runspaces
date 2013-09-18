126c8b790e84316777e65330c8dd90cd68aad09f
Harriet Lawrence <harriet_lawrence@hotmail.com>
2013-09-18 13:06:21 +1000

    Main code

---
+++ Runspaces/RunspaceDemo.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RunspaceDemo.aspx.cs" Inherits="Runspaces.RunspaceDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Runspace Demo</title>
+	<link href="styles/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div class="wrapper">
+        <form id="containerForm" runat="server">
+            <h1>Runspace Demo</h1>
+            <div class="input-area">
+			    <label>Script you want to run:</label>
+                <asp:FileUpload ID="fileUpload" runat="server" />
+			    <div class="clearfix"></div>
+                <label>Test Value 1:</label>
+                <asp:TextBox ID="firstValue" runat="server"></asp:TextBox>
+                <div class="clearfix"></div>
+                <label>Test Value 2:</label>
+                <asp:TextBox ID="secondValue" runat="server"></asp:TextBox>
+                <div class="clearfix"></div>
+                <asp:Button ID="BtnExecuteScript" runat="server" Text="Run Scripts" OnClick="BtnExecuteScript_Click" />
+		    </div>
+            <div class="output-area">
+			    Script output:
+			    <asp:TextBox ID="TxtOutput" runat="server" TextMode="MultiLine" ReadOnly="true"></asp:TextBox>
+			    <asp:Button ID="BtnClear" runat="server" Text="Clear All Values and Output" OnClick="BtnClear_Click" CausesValidation="False"/>
+		    </div>
+    
+        </form>
+    </div>
+</body>
+</html>
---
+++ Runspaces/RunspaceDemo.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Runspaces
+{
+    public partial class RunspaceDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        protected void BtnExecuteScript_Click(object sender, EventArgs e)
+        {
+            BtnExecuteScript.Enabled = false;
+
+            Stream str = fileUpload.FileContent;
+            StreamReader sr = new StreamReader(str);
+            string script = sr.ReadToEnd();
+
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+
+            RunspaceInvoke scriptInvoker = new RunspaceInvoke(runspace);
+            scriptInvoker.Invoke("Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned");
+
+            Pipeline pipe = runspace.CreatePipeline();
+            runspace.SessionStateProxy.SetVariable("firstValue", firstValue.Text);
+            runspace.SessionStateProxy.SetVariable("secondValue", secondValue.Text);
+
+            pipe.Commands.AddScript(script);
+            pipe.Commands.Add("Out-String");
+            Collection<PSObject> results = pipe.Invoke();
+
+            runspace.Close();
+
+            // convert the script result into a single string
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (PSObject obj in results)
+            {
+                stringBuilder.AppendLine(obj.ToString());
+            }
+
+            String output = stringBuilder.ToString();
+            TxtOutput.Text = output;
+        }
+
+        protected void BtnClear_Click(object sender, EventArgs e)
+        {
+            firstValue.Text = "";
+            secondValue.Text = "";
+            TxtOutput.Text = "";
+            if (!BtnExecuteScript.Enabled)
+            {
+                BtnExecuteScript.Enabled = true;
+            }
+        }
+    }
+}
